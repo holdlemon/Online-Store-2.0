f484f54209ca414f2a3da4309c6caae2209d350b
holdlemon <d.vajov2010@yandex.ru>
2024-10-10 16:01:33 +0300

    Отредактированы главная страница, контакты и страница одного товара

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.products, name='products'),
     path('contacts/', views.contacts, name='contacts'),
+    path('prod/<int:pk>/', views.products_detail, name='products_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+from catalog.models import Product
+
+
+def products(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products_list.html', context)
 
 def contacts(request):
     if request.method == "POST":
@@ -12,4 +17,9 @@ def contacts(request):
         return HttpResponse(
             f"{name}, указанные Вами телефон и сообщение получены<br>Телефон: {phone}<br>Сообщение: {message}"
         )
-    return render(request, "contacts.html")
+    return render(request, "contacts_content.html")
+
+def products_detail(request, pk):
+    products = Product.objects.get(pk=pk)
+    context = {'products': products}
+    return render(request, 'products_detail.html', context)
