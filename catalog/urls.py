dfe444872147c662c659e416842ff01976e4295d
holdlemon <d.vajov2010@yandex.ru>
2024-11-16 21:03:24 +0300

    Настроено кеширование для страницы, которая отображает информацию об одном продукте. Добавлено низкоуровневое кеширование для списка продуктов.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ContactsView, ProductDetailView, ProductCreateView, ProductUpdateView, \
-    ProductDeleteView, ProductUnpublishView
+    ProductDeleteView, ProductUnpublishView, ProductByCategoryListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='products_list'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('prod/<int:pk>/', ProductDetailView.as_view(), name='products_detail'),
+    path('prod/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='products_detail'),
     path('prod/create/', ProductCreateView.as_view(), name='products_create'),
     path('prod/<int:pk>/update/', ProductUpdateView.as_view(), name='products_update'),
     path('prod/<int:pk>/delete/', ProductDeleteView.as_view(), name='products_delete'),
     path('prod/<int:pk>/unpublish/', ProductUnpublishView.as_view(), name='products_unpublish'),
+    path('prod/category/', ProductByCategoryListView.as_view(), name='product_by_category_list')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,4 @@
-from gc import get_objects
-from itertools import product
+from django.core.cache import cache
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseForbidden
@@ -7,14 +6,20 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView, View
 
 from catalog.forms import ProductForm
-from catalog.models import Product
+from catalog.models import Product, Category
+from catalog.services import products_by_category
 
 
 class ProductListView(ListView):
     model = Product
 
     def get_queryset(self):
-        return Product.objects.filter(is_published=True)
+        products = cache.get('products')
+        if not products:
+            products = Product.objects.all()
+            cache.set('products', products, 60)
+
+        return products.filter(is_published=True)
 
 
 class ProductDetailView(DetailView):
@@ -54,17 +59,15 @@ class ProductDeleteView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy("catalog:products_list")
 
     def dispatch(self, request, *args, **kwargs):
-        # Получаем объект продукта
+
         product = super().get_object()
-        # Проверяем, является ли текущий пользователь владельцем продукта
+
         if product.owner == self.request.user or request.user.has_perm(
             "catalog.delete_product"
         ):
             return super().dispatch(request, *args, **kwargs)
 
         return HttpResponseForbidden("Вы не можете удалить этот продукт.")
-
-
 
 
 class ProductUnpublishView(LoginRequiredMixin, View):
@@ -77,6 +80,23 @@ class ProductUnpublishView(LoginRequiredMixin, View):
         product.is_published = False
         product.save()
         return redirect('catalog:products_list')
+
+
+class ProductByCategoryListView(ListView):
+    model = Product
+    template_name = "catalog/product_by_category_list.html"
+
+    def get_queryset(self):
+        category_id = self.request.GET.get("category")
+
+        return products_by_category(category_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.all()
+        context["categories"] = categories
+
+        return context
 
 
 class ContactsView(LoginRequiredMixin, TemplateView):
